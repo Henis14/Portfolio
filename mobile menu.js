759f3191b2e357976dc26fbf9d52905aa09374ac
Henis Dcosta <122615082+Henis14@users.noreply.github.com>
2024-05-29 18:31:17 +0530

    mobile

---
+++ mobile menu.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var navToggle = document.getElementById('nav-toggle');
+    var navMenu = document.getElementById('nav-menu');
+
+    navToggle.addEventListener('click', function() {
+        navMenu.classList.toggle('show');
+    });
+
+    // Add click event listeners to all menu items
+    var menuItems = navMenu.querySelectorAll('li a');
+    menuItems.forEach(function(item) {
+        item.addEventListener('click', function() {
+            navMenu.classList.remove('show');
+        });
+    });
+});
