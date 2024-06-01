4843527bd114c91dc552958f1ac5454f643d3606
Henis Dcosta <122615082+Henis14@users.noreply.github.com>
2024-06-01 12:23:32 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -23,15 +23,22 @@ function scrollToTop() {
 }
 
 // Function to toggle visibility of the scroll to top button based on scroll position
-window.onscroll = function() {
-    var button = document.getElementById("scrollToTopButton");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        button.style.display = "block";
-    } else {
-        button.style.display = "none";
-    }
-};
+const scrollToTopButton = document.getElementById('scrollToTopButton');
 
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 200) {
+                scrollToTopButton.style.display = 'block';
+            } else {
+                scrollToTopButton.style.display = 'none';
+            }
+        });
+
+        scrollToTopButton.addEventListener('click', () => {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        });
 
 
 
